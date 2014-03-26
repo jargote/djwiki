6f29dc9a6e115c8ecc464fb81f01b7d9e471f27b
Javier <javier@argotmediagroup.com>
2014-03-26 07:46:01 +0000

    Added url field to WikiPage model class.

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ class WikiPage(models.Model):
     """This class models a Wiki Page.
 
     Attributes:
+        url: String representing the WikiPage's instance url.
         title: String representing the wiki page's title.
         body: Text to format as Html using Markdown.
         author: User who's created the WikiPage model instance.
@@ -15,7 +16,8 @@ class WikiPage(models.Model):
             last updated.
     """
 
-    title = models.CharField('Title', max_length=150, required=True)
+    url = models.CharField('Url', max_length=150)
+    title = models.CharField('Title', max_length=150)
     body = models.TextField('Body', blank=True)
     author = models.ForeignKey(User, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -39,7 +41,7 @@ class Changelog(models.Model):
             was made.
     """
 
-    comments = models.TextField('Comments', required=True)
+    comments = models.TextField('Comments')
     page = models.ForeignKey(WikiPage)
     author = models.ForeignKey(User, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
