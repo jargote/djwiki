9fee3fcaaef746a3ab04f2ec9c31967fdb162347
Javier <javier@argotmediagroup.com>
2014-03-26 15:56:38 +0000

    Stripping tags from WikiPage body.

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.html import strip_tags
 
 
 class WikiPageForm(forms.Form):
@@ -7,3 +8,9 @@ class WikiPageForm(forms.Form):
 
     body = forms.CharField(label='Page Body', widget=forms.Textarea)
     comments = forms.CharField(label='Comments', widget=forms.Textarea)
+
+    def clean_body(self):
+        """Cleaning body value from malicious code."""
+        body = self.data['body']
+
+        return strip_tags(body)
