ae5a4d26722c606bbd5b085af8bed33273315e12
Javier <javier@argotmediagroup.com>
2014-03-26 11:25:17 +0000

    Implemented TestCase (test_title) for WikiPage model.

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from core.models import WikiPage
 
-# Create your tests here.
+
+class WikiPageTest(TestCase):
+
+    def test_title(self):
+        """Testing that a wiki page title is generated correctly from its url
+            value.
+        """
+
+        page = WikiPage.objects.create(url='Dummy_TestPage/For/Fairfax')
+
+        # Testing that Wikipage.title property converts a CamelCase properly.
+        page.url = 'HumanBody'
+        self.assertEqual('Human Body', page.title)
+
+        # Testing that Wikipage.title property converts a underscore properly.
+        page.url = 'human_body'
+        self.assertEqual('Human Body', page.title)
+
+        # Testing that Wikipage.title property converts a CamelCase/underscore
+        # properly.
+        page.url = 'Parts_of_the_HumanBody'
+        self.assertEqual('Parts Of The Human Body', page.title)
+
+        # Testing that Wikipage.title property converts a CamelCase/slash
+        # properly.
+        page.url = 'HumanBody/Parts'
+        self.assertEqual('Human Body / Parts', page.title)
+
+
