c5f9337ab1c11541a563cb877d6c34b811e10350
Javier <javier@argotmediagroup.com>
2014-03-27 11:52:05 +0000

    Using regular expression to match urls.

--- djwiki/urls.py
+++ djwiki/urls.py
@@ -6,15 +6,17 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
 
-    # Change log views
-    url(r'^changelog/$', 'core.views.changelog', name='global_changelog'),
-
     # Page Views
     url(r'^$', 'core.views.index', name='wiki_index'),
-    url(r'^(?P<page_url>\w+)/$', 'core.views.view_page', name='view_page'),
-    url(r'^(?P<page_url>\w+)/edit/$', 'core.views.edit_page', name='edit_page'),
-    url(r'^(?P<page_url>\w+)/changelog/$', 'core.views.changelog',
-        name='changelog'),
+    url(r'^wiki/(?P<page_url>[a-zA-Z0-9_/]+)/$', 'core.views.view_page',
+        name='view_page'),
+    url(r'^wiki(?P<page_url>[a-zA-Z0-9_/]+)/edit/$', 'core.views.edit_page',
+        name='edit_page'),
+
+    # Change log views
+    url(r'^changelog/$', 'core.views.changelog', name='global_changelog'),
+    url(r'^changelog/(?P<page_url>[a-zA-Z0-9_/]+)/$',
+        'core.views.changelog', name='changelog'),
 
     # Admin site urls.
     url(r'^admin/', include(admin.site.urls)),
