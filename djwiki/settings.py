2e7182a3aa9f48be3ee60bb18c869e28143040fb
Javier <javier@argotmediagroup.com>
2014-03-27 11:52:35 +0000

    Added STATICFILES_DIRS conf variable.

--- djwiki/settings.py
+++ djwiki/settings.py
@@ -78,9 +78,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.6/howto/static-files/)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # Markdown files location
 MARKDOWN_ROOT = os.path.join(BASE_DIR, 'core/markdown/')
